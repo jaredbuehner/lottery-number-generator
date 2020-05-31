1edc6042d18c1e767d0f0d21358d0f013ef1cf3b
Jared Buehner <56742442+jaredbuehner@users.noreply.github.com>
2020-05-31 14:10:30 -0400

    Add files via upload

--- Lottery.py
+++ Lottery.py
@@ -1,25 +1,29 @@
 # Lottery.py
+# Import.
 import random
 
 # Start prompt.
 print('\n**** Welcome to the Pick-3, Pick-4 lottery number generator! ****')
 
+# Menu prompt.
 def menu():
     print('\n\nSelect from the following menu:\n\n1. Generate 3-Digit Lottery number\
     \n2. Generate 4-Digit Lottery number\n3. Exit the Application\n')
 
+# Holds loop status in memory.
 loop = True
 
+# Sentinel while loop
 while loop:
     menu()
     userInput = int(input())
+    
 # If-else statements performs proper operation for the user.
     if userInput == 1:
         print('\nYou selected 1. The following 3-digit lottery number was generated:\
             \n')
         for i in range(3):
             print(random.randrange(1, 10), end = '')
-        userInput = 1
         
     if userInput == 2:
         print('\nYou selected 2. The following 4-digit lottery number was generated:\
@@ -30,4 +34,4 @@ while loop:
     if userInput == 3:
         print('\nYou selected 3.\n\nThanks for trying the Lottery Application.\
             \n\n*********************************************************\n')
-        loop = False
+        loop = False # Ends loop and consequently, the program.
